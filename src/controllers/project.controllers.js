6fd74e893faccba6db80a26c3abf11dba50e8389
TanmoyD24 <dastanmoy200224@gmail.com>
2026-07-02 15:46:43 +0530

    feat: implement project management controller with CRUD operations

---
+++ src/controllers/project.controllers.js
@@ -0,0 +1,103 @@
+import { User } from "../models/user.models.js";
+import { Project } from "../models/project.models.js";
+import { ProjectMember } from "../models/projectmember.models.js";
+import { Task } from "../models/task.models.js";
+import { SubTask } from "../models/subtask.models.js";
+import { ProjectNote } from "../models/note.models.js";
+import { ApiResponse } from "../utils/api-response.js";
+import { ApiError } from "../utils/api-error.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import {
+  emailVerificationMailgenContent,
+  forgotPasswordMailgenContent,
+  sendEmail,
+} from "../utils/mail.js";
+import mongoose from "mongoose";
+import { UserRolesEnum } from "../utils/constants.js";
+
+const getProjects = asyncHandler(async (req, res) => { });
+
+const getProjectById = asyncHandler(async (req, res) => { });
+
+const createProject = asyncHandler(async (req, res) => {
+  const { name, descrption } = req.body
+
+  await Project.create({
+    name,
+    description,
+    createdBy: new mongoose.Types.ObjectId(req.user._id),
+  });
+
+  await ProjectMember.create(
+    {
+      user: new mongoose.Types.ObjectId(req.user._id),
+      project: new mongoose.Types.ObjectId(project._id),
+      role: UserRolesEnum.ADMIN
+    }
+  )
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, project, 'Project created successfully'));
+
+});
+
+const updateProject = asyncHandler(async (req, res) => {
+  const { name, description } = req.body
+  const { projectId } = req.params
+  
+  const project = await Project.findByIdAndUpdate(
+    projectId,
+    {
+      name,
+      description
+    },
+    { new: true }
+  );
+
+  if (!project) {
+    throw new ApiError(404, "PROJECT NOT FOUND")
+  }
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        project,
+        "Project updated successfully"
+      )
+    )
+});
+
+const deleteProject = asyncHandler(async (req, res) => {
+  const { projectId } = req.params
+  
+  const project = await Project.findByIdAndDelete(projectId)
+  if (!project) {
+    throw new ApiError(404, "PROJECT NOT FOUND");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, project, "Project updated successfully"));
+});
+
+const addMemberToProject = asyncHandler(async (req, res) => { });
+
+const updateMemberRole = asyncHandler(async (req, res) => { });
+
+const getProjectMembers = asyncHandler(async (req, res) => { });
+
+const deleteMember = asyncHandler(async (req, res) => {});
+
+export {
+  updateMemberRole,
+  addMemberToProject,
+  deleteMember,
+  getProjectMembers,
+  getProjectById,
+  getProjects,
+  createProject,
+  updateProject,
+  deleteProject,
+  updateMemberRole,
+};
