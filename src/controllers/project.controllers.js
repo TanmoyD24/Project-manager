4ad3340143a7e94e97ab1e1a9e6024bdd7931b2b
TanmoyD24 <dastanmoy200224@gmail.com>
2026-07-02 16:12:34 +0530

    feat: implement getProjects function to fetch user-specific projects with member count

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -15,7 +15,61 @@ import {
 import mongoose from "mongoose";
 import { UserRolesEnum } from "../utils/constants.js";
 
-const getProjects = asyncHandler(async (req, res) => { });
+const getProjects = asyncHandler(async (req, res) => {
+  const projects = await ProjectMember.aggregate([
+    {
+      $match: {
+        user: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "projects",
+        localField: "projects",
+        foreignField: "_id",
+        as: "projects",
+        pipeline: [
+          {
+            $lookup: {
+              from: "projectmembers",
+              localField: "_id",
+              foreignField: "projects",
+              as:"projectmembers",
+            },
+          },
+          {
+            $addFields: {
+              members:{
+                $size: "$projectmembers",
+              },
+            },
+          },
+        ],
+      },
+    },
+    {
+      $unwind: "$project"
+    },
+    {
+      $project: {
+        project: {
+          _id: 1,
+          name: 1,
+          description: 1,
+          members: 1,
+          createdAt: 1,
+          createdBy: 1
+        },
+        role: 1,
+        _id: 0
+      }
+    }
+  ]);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, projects, "Project fetched successfully"));
+});
 
 const getProjectById = asyncHandler(async (req, res) => { });
 
@@ -78,7 +132,7 @@ const deleteProject = asyncHandler(async (req, res) => {
   }
   return res
     .status(200)
-    .json(new ApiResponse(200, project, "Project updated successfully"));
+    .json(new ApiResponse(200, project, "Project deleted successfully"));
 });
 
 const addMemberToProject = asyncHandler(async (req, res) => { });
