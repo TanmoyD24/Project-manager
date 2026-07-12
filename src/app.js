62fe061fd3a884e6c17e871b531fd2584efd521c
TanmoyD24 <dastanmoy200224@gmail.com>
2026-07-12 17:59:49 +0530

    add global error handler for improved error responses

--- src/app.js
+++ src/app.js
@@ -36,4 +36,16 @@ app.get("/", (req, res) => {
     res.send("Welcome to basecamp")
 })
 
+// global error handler
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    statusCode,
+    success: false,
+    message,
+    errors: err.errors || [],
+  });
+});
+
 export default app;
